6f9ed40a976364cc158d6c21b6101bd8cc63789b
RimuRimuh <68438095+RimuhRimu@users.noreply.github.com>
2023-04-07 18:22:06 -0400

    Add brands to footer

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -14,8 +14,8 @@ export default function Footer() {
           <Discord />
         </div>
         <div className={styles.poweredBy}>
-          <h5>Powered by vercel <Image src={vercel} height={20} alt="powered by vercel" /> </h5>
-          <h5> and made with <Image src={next} height={20} alt="made with next.js" /> </h5>
+          <h5>Powered by vercel <Image width={80} height={80} src={vercel} alt="powered by vercel" /> </h5>
+          <h5> and made with <Image width={80} height={80} src={next} alt="made with next.js" /> </h5>
         </div>
       </footer>
     </>
