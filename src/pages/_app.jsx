3242fe2ccfd57c0f485c30007cf3abf89c0a7533
RimuhRimu <rimuhrimu@protonmail.com>
2023-04-28 00:10:52 -0400

    Fix build

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,5 @@
 import '@/globals.css'
 import { ThemeProvider } from '@/themes'
-import { LangProvider } from '@/lang'
 import { DM_Sans } from 'next/font/google'
 
 const DMS = DM_Sans({ subsets: ['latin'], weight: '400', fallback: ["Helvetica"] })
@@ -8,11 +7,9 @@ const DMS = DM_Sans({ subsets: ['latin'], weight: '400', fallback: ["Helvetica"]
 export default function App({ Component, pageProps }) {
   return (
     <div className={DMS.className}>
-      <LangProvider>
-        <ThemeProvider>
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </LangProvider>
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </div>
   )
 }
